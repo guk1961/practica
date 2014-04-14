2d3d61691c335fda029fd0feba49c306cb99d460
Yuri_Zhlz <Yuri_Zhlz@SB2536.prepress.dmir.ru>
2014-04-14 15:55:17 +0400

    2

--- mysite/src/ru/guimi/home/Home.java
+++ mysite/src/ru/guimi/home/Home.java
@@ -4,6 +4,6 @@ public class Home {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Привет!");
+		System.out.println("Привет, превед!");
 	}
 }
