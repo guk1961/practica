af53fc9c0ea3d25acebf9bf6adf6b040181daf16
guk1961 <Crba2009>
2014-05-14 18:08:09 +0400

    2

--- monitors/src/ru/adedit/controller/HomeController.java
+++ monitors/src/ru/adedit/controller/HomeController.java
@@ -1,5 +1,35 @@
 package ru.adedit.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+ 
+@Controller
+//@RequestMapping(value = "/home")//, method = RequestMethod.GET)
 public class HomeController {
 
+
+    
+    	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	    public String home(Model model) {
+        model.addAttribute("selected", "home");
+//        model.addAttribute("content", "welcome.vm");
+        return "home";
+        
+    }    
+    	@RequestMapping(value = "/")
+        public String gohome(ModelMap model) {
+
+    		System.out.println("redirect home");
+            return "redirect:/home";
+//            return "redirect:/angular";
+        }
+
+    	
 }
